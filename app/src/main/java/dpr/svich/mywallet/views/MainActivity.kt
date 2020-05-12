95e7b26549c9a234d36f49e05e9ccca6cdb2af34
EugenAl <zheka2142@gmail.com>
2020-05-12 14:07:27 +0300

    Add chart(#11)

--- app/src/main/java/dpr/svich/mywallet/views/MainActivity.kt
+++ app/src/main/java/dpr/svich/mywallet/views/MainActivity.kt
@@ -1,8 +1,10 @@
 package dpr.svich.mywallet.views
 
 import android.content.Intent
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.CalendarContract
 import android.util.Log
 import android.widget.Adapter
 import android.widget.LinearLayout
@@ -14,6 +16,15 @@ import androidx.recyclerview.widget.DefaultItemAnimator
 import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.github.mikephil.charting.animation.Easing
+import com.github.mikephil.charting.charts.BarChart
+import com.github.mikephil.charting.charts.LineChart
+import com.github.mikephil.charting.components.XAxis
+import com.github.mikephil.charting.components.YAxis
+import com.github.mikephil.charting.data.BarData
+import com.github.mikephil.charting.data.BarDataSet
+import com.github.mikephil.charting.data.BarEntry
+import com.github.mikephil.charting.data.Entry
 import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
@@ -35,6 +46,8 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var listDataset: MutableLiveData<ArrayList<Transaction>>
 
+    private val TRANSACTIONS = "Transactions"
+
     val userId = FirebaseAuth.getInstance().currentUser?.uid
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -43,6 +56,29 @@ class MainActivity : AppCompatActivity() {
 
         val user = FirebaseAuth.getInstance().currentUser
         updateUI(user)
+
+        // BarChart init
+        val chartView = findViewById<BarChart>(R.id.mainChartView)
+        chartView.setScaleEnabled(false)
+        chartView.setTouchEnabled(false)
+        chartView.description = null
+        chartView.legend.isEnabled = false
+        // BarChart axis
+        chartView.axisRight.isEnabled = false
+        val chartYAxis = chartView.getAxis(YAxis.AxisDependency.LEFT)
+        chartYAxis.setDrawGridLines(false)
+        chartYAxis.setDrawAxisLine(false)
+        chartYAxis.setDrawLabels(false)
+        val chartXAxis = chartView.xAxis
+        chartXAxis.position = XAxis.XAxisPosition.BOTTOM
+        chartXAxis.textColor = Color.WHITE
+        chartXAxis.setDrawGridLines(false)
+        chartXAxis.setDrawAxisLine(false)
+
+        // TextView init
+        val spendMonthTV = findViewById<TextView>(R.id.month_spend_tv)
+        val spendAverageTV = findViewById<TextView>(R.id.average_spend_tv)
+
 
         // Backdrop
         val holder = findViewById<LinearLayout>(R.id.holderLayout)
@@ -78,7 +114,7 @@ class MainActivity : AppCompatActivity() {
             override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
                 val position = viewHolder.adapterPosition
                 listDataset.value?.get(position)?.id?.let {
-                    Firebase.database.reference.child(userId.orEmpty()).child("Transactions")
+                    Firebase.database.reference.child(userId.orEmpty()).child(TRANSACTIONS)
                         .child(SimpleDateFormat("yyyy/MM/dd", Locale.getDefault()).format(Date()))
                         .child(it).removeValue()
                 }
@@ -89,13 +125,13 @@ class MainActivity : AppCompatActivity() {
         itemTouchHelper.attachToRecyclerView(recycleTransactionView)
 
         // today transaction listener
+        val todayDate = Date()
         var sym: Int
         val symTextView = findViewById<TextView>(R.id.priceTV)
-        val database = Firebase.database.reference.child(userId.orEmpty()).child("Transactions")
-            .child(SimpleDateFormat("yyyy/MM/dd", Locale.getDefault()).format(Date()))
+        val database = Firebase.database.reference.child(userId.orEmpty()).child(TRANSACTIONS)
+            .child(SimpleDateFormat("yyyy/MM/dd", Locale.getDefault()).format(todayDate))
         database.addValueEventListener(object : ValueEventListener{
             override fun onCancelled(p0: DatabaseError) {
-                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
             }
 
             override fun onDataChange(p0: DataSnapshot) {
@@ -104,7 +140,7 @@ class MainActivity : AppCompatActivity() {
                 Log.d("DataSnapshot", "Root: ${p0.key}")
                 for(post in p0.children){
                     Log.d("DataSnapshot", "${post.key}")
-                    var trans = post.getValue(Transaction::class.java)
+                    val trans = post.getValue(Transaction::class.java)
                     trans.let {
                         if(trans!!.isSpend!!) {
                             sym += trans.price!!.toInt()
@@ -120,7 +156,42 @@ class MainActivity : AppCompatActivity() {
                 listDataset.value = dataArray
             }
         })
+        // month transaction listener
+        val monthDatabase = Firebase.database.reference.child(userId.orEmpty()).child(TRANSACTIONS)
+            .child(SimpleDateFormat("yyyy/MM", Locale.getDefault()).format(todayDate))
+            .addValueEventListener(object: ValueEventListener{
+                override fun onCancelled(p0: DatabaseError) {
 
+                }
+
+                override fun onDataChange(p0: DataSnapshot) {
+                    val chartEntries = ArrayList<BarEntry>()
+                    var monthSpend = 0
+                    for(snapshot in p0.children){
+                        Log.d("DataSnapshot", "Day: ${snapshot.key}")
+                        chartEntries.add(BarEntry(snapshot.key!!.toFloat(), 0f))
+                        for(post in snapshot.children){
+                            val price = (post.getValue(Transaction::class.java))?.price?.toInt()!!
+                            chartEntries[chartEntries.lastIndex].y += price
+                            monthSpend += price
+                        }
+                    }
+                    val avg = monthSpend/(SimpleDateFormat("dd",
+                        Locale.getDefault()).format(todayDate)).toInt()
+                    val chartSet = BarDataSet(chartEntries, SimpleDateFormat("MMM",
+                        Locale.getDefault()).format(todayDate))
+                    chartSet.color = resources.getColor(R.color.colorDeepOrange)
+                    chartSet.axisDependency = YAxis.AxisDependency.LEFT
+                    chartSet.valueTextColor = Color.WHITE
+                    val chartData = BarData(chartSet)
+                    chartView.data = chartData
+                    chartView.invalidate()
+                    chartView.animateY(500, Easing.EaseInBack)
+
+                    spendMonthTV.text = getString(R.string.month_spend, monthSpend)
+                    spendAverageTV.text = "Average spend: ${avg}\u20BD"
+                }
+            })
 
         listDataset.observe(this, androidx.lifecycle.Observer {
             adapter.setData(it)
@@ -133,4 +204,5 @@ class MainActivity : AppCompatActivity() {
             startActivity(intent)
         }
     }
+
 }
